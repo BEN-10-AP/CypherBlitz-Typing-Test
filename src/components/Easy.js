d87a67efa4030a1e20185cdc0c7656590834411b
BEN-10-AP <ar56@iitbbs.ac.in>
2023-08-20 23:40:08 +0530

    Third Commit

--- src/components/Easy.js
+++ src/components/Easy.js
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import { CenterFocusStrong, Flare } from '@mui/icons-material';
 
 const Easy = () => {
-  const data='In ands as a testament to the potential of simplicity in transforming our lives. Its benefits, spanning from stress reduction to improved focus and emotional regulation, are far-reaching. As the world becomes increasingly fast-paced, carving out moments to engage in mindful breathing can be a powerful act of self-care, leading to a more balanced and mindful existence.';
+  const data='In ands as a testament to the potential of simplicity in transforming our lives. Its benefits, spanning from stress reduction to improved focus and emotional regulation, are far-reaching. As the world becomes increasingly fast-paced, carving out moments to engage in mindful breathing can be a powerful act of self-care, leading to a more balanced and mindful existence. Repository is created';
 
   return (
     <>
